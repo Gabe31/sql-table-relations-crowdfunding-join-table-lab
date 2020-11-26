4d1c3a3eff795828b5d4bb2e4a6708ad9775bf8c
Gabe Aguilar <gabe.p.aguilar@gmail.com>
2020-11-25 21:02:07 -0500

    Update insert.sql

---
+++ lib/insert.sql
@@ -0,0 +1,31 @@
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES
+
+INSERT INTO users (name, age) VALUES ("Gabe Aguilar", 33)
+INSERT INTO users (name, age) VALUES ("Victoria Aguilar", 38)
+INSERT INTO users (name, age) VALUES ("Andrea Salazar", 27)
+INSERT INTO users (name, age) VALUES ()
+INSERT INTO users (name, age) VALUES
+INSERT INTO users (name, age) VALUES
+INSERT INTO users (name, age) VALUES
+INSERT INTO users (name, age) VALUES
+INSERT INTO users (name, age) VALUES
+INSERT INTO users (name, age) VALUES
+INSERT INTO users (name, age) VALUES
+INSERT INTO users (name, age) VALUES
+INSERT INTO users (name, age) VALUES
+INSERT INTO users (name, age) VALUES
+INSERT INTO users (name, age) VALUES
+INSERT INTO users (name, age) VALUES
+INSERT INTO users (name, age) VALUES
+INSERT INTO users (name, age) VALUES
+INSERT INTO users (name, age) VALUES
+INSERT INTO users (name, age) VALUES
